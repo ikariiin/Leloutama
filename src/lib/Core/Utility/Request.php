17e4868b8e5dfbc1c924a11b0433a35ec2c7558f
gourabNagDev <gourabnag12@gmail.com>
2016-05-06 12:51:57 +0530

    new implementation of callback functions, status: failing

---
+++ src/lib/Core/Utility/Request.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gourab
+ * Date: 4/5/16
+ * Time: 9:43 PM
+ */
+
+namespace Leloutama\lib\Core\Utility;
+
+
+class Request
+{
+
+}
---
+++ src/lib/Core/Utility/Response.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gourab
+ * Date: 4/5/16
+ * Time: 9:59 PM
+ */
+
+namespace Leloutama\lib\Core\Utility;
+
+
+class Response
+{
+
+}
